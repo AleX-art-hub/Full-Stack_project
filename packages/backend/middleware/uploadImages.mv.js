25045787d4ef0713f6d21c3565920a444b46e73d
BodNS <bondarenokn@gmail.com>
2021-05-22 13:59:12 +0300

    backend

--- packages/backend/middleware/uploadImages.mv.js
+++ packages/backend/middleware/uploadImages.mv.js
@@ -1,3 +1,5 @@
 const multer = require('multer');
 const path = require('path');
+const {staticPath} = require('./../config/config');
+const upload = multer({dest: path.basename(staticPath, '/images')});
 module.exports = upload;
