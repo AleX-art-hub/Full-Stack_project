0cc213656466af473b8aa0b4c6477bf55a9d174f
BodNS <bondarenokn@gmail.com>
2021-05-22 12:41:58 +0300

    backend: superheroRouter

--- packages/backend/routers/superheroRouter.js
+++ packages/backend/routers/superheroRouter.js
@@ -1,4 +1,5 @@
 const {Router}  = require('express');
+const {superheroController} = require('./../controllers');
 const superheroRouter = Router();
 superheroRouter.route('/').post(superheroController.create).get(superheroController.getMany);
 superheroRouter.route('/:superheroId').get(superheroController.getById).put(superheroController.updateById, superheroController.create).delete(superheroController.deleteById);
