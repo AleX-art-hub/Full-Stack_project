7c18a3963e571d96de87aee4084a67377535d304
BodNS <bondarenokn@gmail.com>
2021-05-23 12:28:58 +0300

    backend: superhero

--- packages/backend/config/config.js
+++ packages/backend/config/config.js
@@ -1,4 +1,4 @@
 const path = require('path');
 module.exports = {
-  staticPath: process.env.STATIC_PATH ?? path.join(__dirname, '../public'),
+  staticPath: process.env.STATIC_PATH ?? path.join(__dirname, './public'),
 };
--- packages/backend/middleware/uploadImages.mv.js
+++ packages/backend/middleware/uploadImages.mv.js
@@ -1,11 +1,10 @@
-
 const multer = require('multer');
 const path = require('path');
 const { staticPath } = require('./../config/config');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(staticPath, '/images'));
+    cb(null, path.join(staticPath + '/images'));
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '_' + file.originalname);
